ffe99c42dc2a588f60b320354ecb92c1650205a2
Alexander Zhernakov <neongenesisdatabase@gmail.com>
2024-10-16 00:35:55 +0500

    Added input and reading from file

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,38 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        for (String str : args) {
-            System.out.println("My arguments = " + str);
-        }
+        String result = CheckForAWord(TakeADictionary(),InputWord());
+        System.out.printf("Результат: %s", result);
     }
+
+    public static String InputWord() {
+        Scanner in = new Scanner(System.in);
+        return in.nextLine();
+    }
+
+    public static ArrayList<String> TakeADictionary() {
+        ArrayList<String> wordsDict = new ArrayList<>();
+        try(BufferedReader br = new BufferedReader(new FileReader("D:\\JavaProjects\\hangman\\dictionary.txt")))
+        {
+            String word;
+            while ((word = br.readLine())!=null){
+                wordsDict.add(word);
+            }
+        }
+        catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        return wordsDict;
+    }
+
+    public static String CheckForAWord(ArrayList<String> wordsDict, String word) {
+        if(wordsDict.contains(word)) return "Бинго!!!";
+        else return "Лузер!!!";
+    }
+
 }
