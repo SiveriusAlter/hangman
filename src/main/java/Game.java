0c8f0536e9fc6dfee812a38e1ad1fa319ae0e641
Alexander Zhernakov <neongenesisdatabase@gmail.com>
2024-10-22 23:29:30 +0500

    Добавил логику скрытия и открытия символов.

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -4,6 +4,7 @@ public class Game {
         CheckResult result;
         String inputWord;
         Picture[] pictures = Picture.values();
+        String hiddenWord = HidingWord(randomWord);
         for(int i = 0; i < pictures.length;) {
             System.out.println("Введите букву или слово целиком:\n");
             inputWord = Input.InputWord(randomWord.length());
@@ -12,9 +13,12 @@ public class Game {
                 Output.ResultOut(result, randomWord);
                 break;
             }
-            else if (result == CheckResult.CONTAIN) Output.ResultOut(result, randomWord);
+            else if (result == CheckResult.CONTAIN) {
+                hiddenWord = OpeningLetters(hiddenWord,randomWord,inputWord);
+                Output.ResultOut(result, hiddenWord);
+            }
             else {
-                Output.ResultOut(result, randomWord, pictures[i]);
+                Output.ResultOut(result, hiddenWord, pictures[i]);
                 i++;
             }
         }
@@ -24,5 +28,23 @@ public class Game {
         if(randomWord.equals(inputWord)) return CheckResult.WIN;
         else if(randomWord.contains(inputWord)) return CheckResult.CONTAIN;
         else return CheckResult.MISTAKE;
+    }
+
+    private String HidingWord(String randomWord) {
+        StringBuilder hiddenWord = new StringBuilder(randomWord);
+        for(int i = 0; i<randomWord.length(); i++) {
+            hiddenWord.replace(i,i+1,"*");
+        }
+        return hiddenWord.toString();
+    }
+
+    private String OpeningLetters(String hiddenWord, String randomWord, String inputWord) {
+        StringBuilder openWord = new StringBuilder(hiddenWord);
+        for (int i = 0; i<randomWord.length(); i++) {
+            if(inputWord.charAt(0) == randomWord.charAt(i)) {
+                openWord.replace(i,i+1, inputWord);
+            }
+        }
+        return openWord.toString();
     }
 }
